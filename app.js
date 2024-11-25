884c80f5cd0fbc4c72e87b173104b38bdbf61785
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-11-25 12:19:15 +0100

    Added views, routes and app

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const app = express();
+const newMsgRouter = express.Router();
+const messagesRouter = express.Router();
+const path = require("node:path");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`My first Express app - listening on port ${PORT}!`);
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello Worldddd!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+newMsgRouter.get("/", (req, res) => res.render("new"));
+newMsgRouter.post("/", (req, res) => {
+  const { messageUser, messageText } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
 });
+
+messagesRouter.get("/:user", (req, res) => {
+  const user = messages.find((msg) => msg.user === req.params.user);
+  res.render("message", { user: user });
+});
+
+app.use("/new", newMsgRouter);
+app.use("/messages", messagesRouter);
+
+app.get("/", (req, res) => res.render("index", { messages: messages }));
+
+app.listen(3000);
